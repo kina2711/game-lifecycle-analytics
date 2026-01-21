680e2671768a5778ca0a07121b09f6a8b2612bcf
Kien Thai <rabbit.medical2k1@gmail.com>
2026-01-22 01:55:52 +0700

    Update 02_retention.sql

--- sql/02_retention.sql
+++ sql/02_retention.sql
@@ -1,18 +1,29 @@
-CREATE OR REPLACE VIEW `game-lifecycle-analytics.game_lifecycle_analytics.vw_daily_retention` AS
-WITH cohort AS (
-    SELECT uid, DATE(TIMESTAMP_SECONDS(reg_ts)) as cohort_date
-    FROM `game-lifecycle-analytics.game_lifecycle_analytics.reg_data`
+CREATE OR REPLACE VIEW `game-lifecycle-analytics.game_lifecycle_analytics.vw_daily_retention_curve` AS
+WITH user_activity AS (
+    SELECT 
+        t1.uid,
+        DATE_DIFF(DATE(TIMESTAMP_SECONDS(t2.auth_ts)), DATE(TIMESTAMP_SECONDS(t1.reg_ts)), DAY) as days_since_reg
+    FROM `game-lifecycle-analytics.game_lifecycle_analytics.reg_data` t1
+    JOIN `game-lifecycle-analytics.game_lifecycle_analytics.auth_data` t2 ON t1.uid = t2.uid
+    WHERE t2.auth_ts >= t1.reg_ts -- Chỉ lấy log sau khi đăng ký
 ),
-activity AS (
-    SELECT uid, DATE(TIMESTAMP_SECONDS(auth_ts)) as activity_date
-    FROM `game-lifecycle-analytics.game_lifecycle_analytics.auth_data`
+daily_stats AS (
+    SELECT 
+        days_since_reg,
+        COUNT(DISTINCT uid) as retained_users
+    FROM user_activity
+    GROUP BY 1
+),
+total_users AS (
+    SELECT COUNT(DISTINCT uid) as total_count 
+    FROM `game-lifecycle-analytics.game_lifecycle_analytics.reg_data`
 )
-SELECT
-    c.cohort_date,
-    DATE_DIFF(a.activity_date, c.cohort_date, DAY) as day_diff,
-    COUNT(DISTINCT c.uid) as user_count
-FROM cohort c
-JOIN activity a ON c.uid = a.uid
-WHERE a.activity_date >= c.cohort_date
-GROUP BY 1, 2
-ORDER BY 1, 2;
+SELECT 
+    d.days_since_reg,
+    d.retained_users,
+    t.total_count,
+    -- Tính % Retention: (User còn lại / Tổng User ban đầu) * 100
+    ROUND((d.retained_users / t.total_count) * 100,2) as retention_percent
+FROM daily_stats d
+CROSS JOIN total_users t
+ORDER BY 1;
